5c8c142d1040c57ba73d753078f4082aef9fbe73
k1m2njun <k1m2njun@naver.com>
2023-06-26 22:20:16 +0900

    Added Methods of Players

--- src/main/java/BaseBallApplication.java
+++ src/main/java/BaseBallApplication.java
@@ -1,4 +1,5 @@
 import db.DBConnection;
+import model.player.Player;
 import model.player.PlayerDao;
 
 import java.sql.Connection;
@@ -8,13 +9,21 @@ public class BaseBallApplication {
 
     public static void main(String[] args) {
         Connection connection = DBConnection.getInstance();
-        PlayerDao playerDao = new PlayerDao(connection);
+
+        PlayerDao playerDao = PlayerDao.getInstance();
+
+        playerDao.connectDB(connection);
 
         try {
-            playerDao.createPlayerTable();
+//            playerDao.createPlayerTable();
+//            for(Player p : playerDao.getPlayersByTeam(2)) {
+//                System.out.println(p.toString());
+//            }
+            System.out.println(playerDao.getPlayerByPosition("1루수", 2).toString());
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
     }
 }
--- src/main/java/service/PlayerService.java
+++ src/main/java/service/PlayerService.java
@@ -1,4 +1,12 @@
 package service;
 
+import model.player.Player;
+import model.player.PlayerDao;
+
+import java.sql.Connection;
+
 public class PlayerService {
+    private PlayerDao playerDao;
+    private Connection connection;
+
 }
