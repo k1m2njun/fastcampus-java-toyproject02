51df3a4769ba437201df06ed937fb3f76a1fc110
MunJunHo <mjh1995713@gmail.com>
2023-06-29 21:39:50 +0900

    feat:PositionService 생

--- src/main/java/BaseBallApplication.java
+++ src/main/java/BaseBallApplication.java
@@ -59,13 +59,13 @@ public class BaseBallApplication {
                     StadiumService stadiumService = new StadiumService(stadiumDao,connection);
                     stadiumService.registerNewStadium(stadiumName);
                     connection.commit();
-                    flag++;
+
                     continue;
                 }
                 if (order.equals("야구장목록")) {//2.예시 -- 요청 : 야구장목록
                     StadiumService stadiumService = new StadiumService(stadiumDao,connection);
                     stadiumService.getStadiumList();
-                    flag++;
+
                     continue;
                 }
                 if (order.equals("팀등록")) {//3.예시 -- 요청 : 팀등록?stadiumId=4&name=NC
@@ -75,28 +75,34 @@ public class BaseBallApplication {
                     TeamService teamService = new TeamService(stadiumDao,teamDao,connection);
                     teamService.registerNewTeam(stadiumId, teamName);
                     connection.commit();
-                    flag++;
+
                     continue;
                 }
                 if (order.equals("팀목록")) {//4.예시 -- 요청 : 팀목록
                     TeamService teamService = new TeamService(stadiumDao,teamDao,connection);
                     teamService.getTeamList();
+
                     continue;
                 }
                 if (order.equals("선수등록")) {//5.요청 : 선수등록?teamId=1&name=이대호&position=1루수
 
+
                     continue;
                 }
                 if (order.equals("선수목록")) {//6.요청 : 선수목록?teamId=1
+
 
                     continue;
                 }
                 if (order.equals("퇴출등록")) {//7.요청 : 퇴출등록?playerId=1&reason=도박
                     int outPlayerId = Integer.parseInt(words[2]);
                     String outPlayerReason = words[4];
-                    System.out.println(outPlayerId+" "+outPlayerReason);
+                    connection.setAutoCommit(false);
+                    //System.out.println(outPlayerId+" "+outPlayerReason);
                     OutPlayerService outPlayerService = new OutPlayerService(outPlayerDao,playerDao,connection);
                     outPlayerService.registerOutPlayer(outPlayerId, outPlayerReason);
+                    connection.commit();
+
                     continue;
                 }
                 if (order.equals("퇴출목록")) {//8.요청 : 퇴출목록
@@ -105,6 +111,7 @@ public class BaseBallApplication {
                     continue;
                 }
                 if (order.equals("포지션별목록")) {//10.요청 : 포지션별목록
+
 
                     continue;
                 }
--- src/main/java/dto/position/PositionResponseDto.java
+++ src/main/java/dto/position/PositionResponseDto.java
@@ -1,5 +1,40 @@
 package dto.position;
 
 public class PositionResponseDto {
+    private String p;// 투수
+    private String c; // 포수
+    private String b1;// 1루수
+    private String b2;// 2루수
+    private String b3;// 3루수
+    private String ss;// 유격수
+    private String lf;// 좌익수
+    private String cf;//중견수
+    private String rf;//우익수
 
+    public PositionResponseDto(String p, String c, String b1, String b2, String b3, String ss, String lf, String cf, String rf) {
+        this.p = p;
+        this.c = c;
+        this.b1 = b1;
+        this.b2 = b2;
+        this.b3 = b3;
+        this.ss = ss;
+        this.lf = lf;
+        this.cf = cf;
+        this.rf = rf;
+    }
+
+    @Override
+    public String toString() {
+        return "PositionResponseDto{" +
+                "투수='" + p + '\'' +
+                ",포수='" + c + '\'' +
+                ",1루수='" + b1 + '\'' +
+                ",2루수='" + b2 + '\'' +
+                ",3루수='" + b3 + '\'' +
+                ",유격수='" + ss + '\'' +
+                ",좌익수='" + lf + '\'' +
+                ",중견수='" + cf + '\'' +
+                ",우익수='" + rf + '\'' +
+                '}';
+    }
 }
